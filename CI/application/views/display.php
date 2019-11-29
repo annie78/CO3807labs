29b6dd178f771f7427354c7ab31691187c1a35f6
anne <aheaps@uclan.ac.uk>
2019-11-29 04:10:10 -0800

    week8

---
+++ CI/application/views/display.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+  <head>
+    <title>mypage</title>
+  </head>
+<h1>Show Rows</h1>
+<table>
+	<tr>
+<td>ID</td> <td>Name</td> <td>Age</td> 
+	</tr>
+     <?php 
+		foreach ($records as $row){
+		echo '<tr>' ;
+		echo '<td>'.$row->id.'</td>';
+		echo '<td>'.$row->name.'</td>';
+		echo '<td>'.$row->age.'</td>';
+		echo '</tr>';
+		}
+	?>
+</table>
+  <body>
+
+
+  </body>
+</html>
+
+
+
+
---
+++ CI/application/views/mypage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <head>
+    <title>mypage</title>
+  </head>
+<h1> <?php echo $name; ?> </h1>
+<p> <?php echo $email; ?> </p>
+<p> <?php echo $course; ?> </p>
+  <body>
+
+
+  </body>
+</html>
+
+
+
+
+
+
+
+
